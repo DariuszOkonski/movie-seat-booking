2413d0ee2ecb1c8eef7d13fcf1fb129b90ad2d43
Dariusz <darek200180@gmail.com>
2020-11-02 22:52:51 +0000

    add funcionality to count ticket price depends of number of marked seats

--- script.js
+++ script.js
@@ -7,8 +7,18 @@ const movieSelect = document.getElementById('movie');
 // + sign will change to int type
 const ticketPrice = +movieSelect.value;
 
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    
+    const selectedSeatsCount = selectedSeats.length;
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
 container.addEventListener('click', (e) => {
     if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
         e.target.classList.toggle('selected');
+
+        updateSelectedCount();
     } 
 });
